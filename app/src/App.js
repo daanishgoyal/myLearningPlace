f8e47a370d0dec7d24832a0ea13f28c5ab7cde7d
Akshay Ganapathy <akshayg1996@gmail.com>
2022-01-25 23:45:25 -0500

    updated app.js

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./components/navbar";
-import Counters from "./components/counters";
 import HomePage from "./components/homepage";
 
 class App extends Component {
