24448631f5c942302066ba78aeb08673318ad681
Akshay Ganapathy <akshayg1996@gmail.com>
2022-01-29 17:07:31 -0500

    created initial version of homepage

--- app/src/App.js
+++ app/src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./components/navbar";
 import HomePage from "./components/homepage";
+import Footer from "./components/footer";
 
 class App extends Component {
     state = {};
 
     render() {
         return (
-            <div>
+            <div className="bg-image">
                 <NavBar />
                 <main className="container">
                     <HomePage />
                 </main>
+                <Footer />
             </div>
         );
     }
