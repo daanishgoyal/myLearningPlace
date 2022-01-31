91cf19677315704b8ceb7f4d14eb56211b70c59f
Akshay Ganapathy <akshayg1996@gmail.com>
2022-01-30 23:20:20 -0500

    updated navbar to include login dropdown

--- app/src/components/navbar.jsx
+++ app/src/components/navbar.jsx
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import "./navbar.css";
+import { NavLink } from "react-router-dom";
 
 // Stateless Functional Components
-// If you dont need a the full implementation of a class when you can accomplish the same with a lambda, you use SFCs.
+// If you dont need a the full implementation of a className when you can accomplish the same with a lambda, you use SFCs.
 
 const NavBar = (props) => {
     return (
-        <nav className="sticky navbar navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <NavLink className="navbar-brand" to="/">
                     MyLearningPlace
-                </a>
+                </NavLink>
+                <li className="nav-item dropdown">
+                    <NavLink
+                        className="nav-link dropdown-toggle bg-dark text-light"
+                        to="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                    >
+                        Login
+                    </NavLink>
+                    <ul className="dropdown-menu me-5 bg-dark">
+                        <li>
+                            <NavLink className="dropdown-item bg-dark" to="/">
+                                Login
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="dropdown-item bg-dark" to="/">
+                                Sign-up
+                            </NavLink>
+                        </li>
+                    </ul>
+                </li>
             </div>
         </nav>
     );
