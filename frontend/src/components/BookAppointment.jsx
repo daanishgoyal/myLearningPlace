9bfff73ff9120bee9d42e077912e5c3f546bdf5d
Nandigam <y.nandigam@gmail.com>
2022-02-16 21:08:34 -0500

    seperatepagesbookcontact

---
+++ frontend/src/components/BookAppointment.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookAppointment= ()=>
+
+{
+
+
+return
+{
+    <div className= 'book'>
+    <h1>Bookappointment </h1>
+    </div>
+}
+}
+export default BookAppointment;
---
+++ frontend/src/components/ContactDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ContactDetails= ()=>
+
+{
+
+
+return
+{
+    <div className= 'book'>
+    <h1>ContactDetails </h1>
+    </div>
+}
+}
+export default ContactDetails;
