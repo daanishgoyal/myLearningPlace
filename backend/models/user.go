6e2af5290073d6155894e79b697fd8373e4694a0
Aman Pathak <34034237+Aman2909@users.noreply.github.com>
2022-02-24 02:51:05 +0530

    Update user.go

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,13 +1,9 @@
 package models
 
-// Capital columns
-
 type User struct {
 	ID        uint `gorm:"primary_key;NOT NULL AUTO_INCREMENT"`
-	FirstName string
-	LastName  string
-	//City      string
+	FirstName string `gorm:"column: firstName"`
+	LastName  string `gorm:"column: lastName"`
 	Email string `gorm:"UNIQUE; NOT NULL"`
-	//Password  string
 	Password []byte
 }
