a82e02ae416af8fa90bcb4041f31beb3d8bfc262
Daanish Goyal <dg5052@gmail.com>
2022-03-22 23:12:44 -0400

    added constants.go

---
+++ backend/config/constants.go
@@ -0,0 +1,20 @@
+package config
+
+import "time"
+
+const SqlUserName string = "root"
+const SqlPassword string = ""
+const SqlDatabaseName string = "BackEnd"
+
+// Const Map
+func GetWeekEndMap() map[string]time.Weekday {
+	return map[string]time.Weekday{
+		"Sunday":    time.Sunday,
+		"Monday":    time.Monday,
+		"Tuesday":   time.Tuesday,
+		"Wednesday": time.Wednesday,
+		"Thursday":  time.Thursday,
+		"Friday":    time.Friday,
+		"Saturday":  time.Saturday,
+	}
+}
