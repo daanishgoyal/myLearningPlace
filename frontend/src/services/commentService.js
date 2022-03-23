0d47d83ce62a37ebc5e7e1cd75fe71b0b8d6f02b
Akshay Ganapathy <akshayg1996@gmail.com>
2022-03-22 23:43:50 -0400

    updated teacher_id in commentsService

--- frontend/src/services/commentService.js
+++ frontend/src/services/commentService.js
@@ -4,8 +4,8 @@ import config from "../config.json";
 const apiEndpoint = config.apiUrl + "/getComments";
 
 export async function getComments(teacherId) {
-  const { data: comments } = await http.post(apiEndpoint, {
-    id: String(teacherId),
-  });
-  return comments;
+    const { data: comments } = await http.post(apiEndpoint, {
+        teacher_id: String(teacherId),
+    });
+    return comments;
 }
