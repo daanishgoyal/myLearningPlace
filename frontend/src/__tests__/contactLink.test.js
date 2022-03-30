44063e2167ce62b2db6f4a27aacfc46f3be3318c
Yallamandaiah <y.nandigam@gmail.com>
2022-03-30 01:28:48 -0400

    integration tests in cypress for sprint 3

--- frontend/src/__tests__/contactLink.test.js
+++ frontend/src/__tests__/contactLink.test.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import TeacherSpecificDetails from '../components/teachers/TeacherSpecificDetails';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import TeacherSpecificDetails from "../components/teachers/TeacherSpecificDetails";
 
+describe("SearchForm", () => {
+    it("contact details page coming or not", () => {
+        render(<TeacherSpecificDetails />);
 
+        const heading = screen.getByRole("link", {
+            name: /contactdetails/i,
+        });
 
-describe('SearchForm', () => {
-    it('renders the search form', () => {
-      render(<TeacherSpecificDetails />)
-  
-      const heading= screen.getByRole('link', {
-        name: /contactdetails/i
-      })
-      
-      
-  
-      expect(heading).toBeVisible()
-    })
-  })
+        expect(heading).toBeVisible();
+    });
+});
--- frontend/src/__tests__/searchForm.test.js
+++ frontend/src/__tests__/searchForm.test.js
@@ -1,14 +1,14 @@
-import { render, screen } from '@testing-library/react'
-import SearchForm from '../components/searchForm'
+import { render, screen } from "@testing-library/react";
+import SearchForm from "../components/searchForm";
 
-describe('SearchForm', () => {
-  it('renders the search form', () => {
-    render(<SearchForm />)
+describe("SearchForm", () => {
+    it("Heading in the first page is there or not", () => {
+        render(<SearchForm />);
 
-    const heading = screen.getByRole('heading', {
-        name: /search for online\/home tutors/i
-      })
+        const heading = screen.getByRole("heading", {
+            name: /search for online\/home tutors/i,
+        });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+        expect(heading).toBeInTheDocument();
+    });
+});
