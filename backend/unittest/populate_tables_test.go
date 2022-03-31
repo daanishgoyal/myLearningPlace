69fd0a77ebe2daf12d906a49edc3066a74624f9e
Aman Pathak <amanpathak2909@gmail.com>
2022-03-31 00:13:56 -0400

    Updated previous testcases

--- backend/unittest/populate_tables_test.go
+++ backend/unittest/populate_tables_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestPopulateTables(t *testing.T) {
 
-	dsn := config.SqlUserName + ":" + config.SqlPassword + "@/" + config.SqlDatabaseName
+	dsn := config.SqlUserName + ":" + config.SqlPassword + "@/" + config.SqlDatabaseName + "?parseTime=True&loc=Local"
 	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		t.Errorf("Connection to DB Failed")
@@ -25,9 +25,12 @@ func TestPopulateTables(t *testing.T) {
 	var relTS []models.Relation_t_s
 	var miscImage []models.MiscImages
 	var slots []models.Slot
+	var contact []models.ContactDetails
+	var booking []models.Booking
+	var teacherSchedule []models.TeacherSchedule
 
-	tableModels := []interface{}{teachers, users, skills, comments, relTS, miscImage, slots}
-	table := []string{"Teachers", "User", "Skills", "Comments", "RelationTS", "MiscImages", "Slot"}
+	tableModels := []interface{}{teachers, users, skills, comments, relTS, miscImage, slots, contact, booking, teacherSchedule}
+	table := []string{"Teachers", "User", "Skills", "Comments", "RelationTS", "MiscImages", "Slot", "ContactDetails", "Booking", "TeacherSchedule"}
 
 	for i := 0; i < len(tableModels); i++ {
 		count := DB.Find(&tableModels[i])
