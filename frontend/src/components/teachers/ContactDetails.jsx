9e38dd6bfad376cba12d110d9d8813536a3f943b
Akshay Ganapathy <akshayg1996@gmail.com>
2022-04-12 03:06:30 -0400

    email click opens default app for mailbox
    Clicking on email link under contact details of a teacher opens  the default application for mailbox

--- frontend/src/components/teachers/ContactDetails.jsx
+++ frontend/src/components/teachers/ContactDetails.jsx
@@ -1,5 +1,6 @@
 import { React, Component } from "react";
 import { getContactDetails } from "../../services/contactDetailsService";
+import { Link } from "react-router-dom";
 import "./ContactDetails.css";
 class ContactDetails extends Component {
     state = {
@@ -16,16 +17,6 @@ class ContactDetails extends Component {
         } catch {}
     }
 
-    //  sendMail() {
-    //     var link = contactDetails.map((contact, index) => {
-    //         contact.Teacher_Email}))
-    //              + "&subject=" + encodeURIComponent("Appointment Booking")
-    //              + "&body=" + encodeURIComponent(document.getElementById('myText').value)
-    //     ;
-
-    //     window.location.href = link;
-    // }
-
     render() {
         const { contactDetails } = this.state;
         // console.log(contactDetails);
@@ -36,45 +27,27 @@ class ContactDetails extends Component {
                 {contactDetails.map((contact, index) => {
                     return (
                         <div className="text-center" key={index}>
+                            <div className="comment-text font-weight-bold h6">
+                                &#9993;
+                                <Link
+                                    to="#"
+                                    onClick={(e) => {
+                                        window.location =
+                                            "mailto:" + contact.Teacher_Email;
+                                        e.preventDefault();
+                                    }}
+                                >
+                                    {" "}
+                                    {contact.Teacher_Email}
+                                </Link>
+                            </div>
                             <div className="name comment-text font-weight-bold h6">
                                 &#128222; {contact.Teacher_MobileNumber}
-                            </div>
-
-                            <div // onClick={() =>
-                                //     (window.location = {contact.Teacher_Email})
-                                // }
-                                className="comment-text font-weight-bold h6"
-                                // onClick={sendMail()}}
-                            >
-                                &#9993; {contact.Teacher_Email}
                             </div>
                         </div>
                     );
                 })}
             </div>
-
-            //   <div class="container mt-5">
-            //     <div class="d-flex justify-content-center row">
-            //       <div class="col-md-6">
-            //         <div class="p-3 ">
-            //           <h6>Phone number</h6>
-            //           <div class="review mt-4">
-            //             <div class="d-flex flex-row comment-user">
-            //               <div class="ml-2">
-            //                 <div class="d-flex flex-row align-items-center">
-            //                   <span class="name font-weight-bold">Hui jhong</span>
-            //                   <span class="dot"></span>
-            //                 </div>
-            //               </div>
-            //             </div>
-            //             <div class="mt-2">
-            //               <p class="comment-text">98888814536</p>
-            //             </div>
-            //           </div>
-            //         </div>
-            //       </div>
-            //     </div>
-            //   </div>
         );
     }
 }
